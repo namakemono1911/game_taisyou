0c1341a265739fdead1953e0b42108c3f26a0ff9
taktye <hal.ishiitakahiro@gmail.com>
2018-04-20 11:13:41 +0900

    playerContoller

---
+++ Assets/playerContoller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerContoller : MonoBehaviour {
+
+
+    [SerializeField]
+    private Rigidbody2D a;
+
+    [SerializeField]
+    private float b;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 pos = this.transform.position;
+            pos.x += 0.1f;
+            this.transform.position = new Vector3(pos.x , pos.y , pos.z);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 pos = this.transform.position;
+            pos.x -= 0.1f;
+            this.transform.position = new Vector3(pos.x, pos.y, pos.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 aaaaa = new Vector2(0, b);
+            a.AddForce(aaaaa);
+        }
+    }
+}
